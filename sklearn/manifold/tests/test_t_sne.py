33b94d151af64e61172ebb4ed0598cd6c513d906
Chris Moody <cemoody@squareup.com>
2014-12-23 15:43:06 -0800

    draft of new BH t-SNE tests

--- sklearn/manifold/tests/test_t_sne.py
+++ sklearn/manifold/tests/test_t_sne.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.sparse as sp
 from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_almost_equal
+from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_less
 from sklearn.utils.testing import assert_raises_regexp
 from sklearn.utils import check_random_state
@@ -16,6 +17,8 @@ from sklearn.manifold._utils import _binary_search_perplexity
 from scipy.optimize import check_grad
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
+from sklearn.metrics.pairwise import pairwise_distances
+import bhtsne
 
 
 def test_gradient_descent_stops():
@@ -233,6 +236,46 @@ def test_pca_initialization_not_compatible_with_precomputed_kernel():
                          tsne.fit_transform, np.array([[0.0], [1.0]]))
 
 
+def test_answer_gradient_two_particles():
+    """ This case with two particles test the tree with only a single
+        set of children
+    """
+    pos_input = np.array([[1.0, 0.0], [0.0, 1.0]])
+    pos_output = np.array([[-4.961291e-05, -1.072243e-04],
+                           [9.259460e-05, 2.702024e-04]])
+    grad_output = np.array([[-2.37012478e-05, -6.29044398e-05],
+                            [2.37012478e-05, 6.29044398e-05]])
+    test(pos_input, pos_output, grad_output)
+
+
+def test_answer_gradient_four_particles():
+    """ This case with two particles test the tree with
+        multiple levels of children
+    """
+    pos_input = np.array([[1.0, 0.0], [0.0, 1.0],
+                          [5.0, 2.0], [7.3, 2.2]])
+    pos_output = np.array([[6.080564e-05, -7.120823e-05],
+                           [-1.718945e-04, -4.000536e-05],
+                           [-2.271720e-04, 8.663310e-05],
+                           [-1.032577e-04, -3.582033e-05]])
+    grad_output = np.array([[5.81128448e-05, -7.78033454e-06],
+                            [-5.81526851e-05, 7.80976444e-06],
+                            [4.24275173e-08, -3.69569698e-08],
+                            [-2.58720939e-09, 7.52706374e-09]])
+    test(pos_input, pos_output, grad_output)
+
+
+def test(pos_input, pos_output, grad_output, verbose=False, perplexity=0.1):
+    distances = pairwise_distances(pos_input)
+    args = distances, perplexity, verbose
+    pij_input = _joint_probabilities(*args)
+    pij_input = squareform(pij_input)
+
+    grad_bh, grad_exact = bhtsne.quadtree_compute(pij_input, pos_output,
+                                                  verbose=verbose)
+    assert_array_almost_equal(grad_bh, grad_output, decimal=4)
+
+
 def test_verbose():
     random_state = check_random_state(0)
     tsne = TSNE(verbose=2)
@@ -262,4 +305,3 @@ def test_chebyshev_metric():
     tsne = TSNE(verbose=2, metric="chebyshev")
     X = random_state.randn(5, 2)
     tsne.fit_transform(X)
-
